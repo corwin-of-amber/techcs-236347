508546af2b599e82fd7bba106a75fe54c8b24724
Shachar Itzhaky <corwin.amber@gmail.com>
2024-07-22 19:43:59 +0300

    [bugfix] Question numbering mishap.

--- ex2/lists.v
+++ ex2/lists.v
@@ -6,14 +6,16 @@ Print list.
 Print rev.
 
 
-(* Q1. Define is_sorted.  (returns a Prop.)  *)
+(* Q1. Define `is_sorted`.  (should return a Prop.)  *)
 
 
-(* Q2. Show that this list is sorted. *)
+(* Show that this list is sorted. *)
 Lemma warm_up : is_sorted [3;5;9] le.
 
 
-(* Q3. Prove that an ascending list of nat, when reversed, 
+(* Q2. Prove that an ascending list of nat, when reversed, 
  *     becomes a descending list. *)
+
+(*     Don't forget to use the _hint_ when proving it. *)
 
 Theorem rev_asc_desc : ...
